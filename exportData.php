8c2f128b35c0a8c8bf7cbe0fa02c970742060e9d
thinkun <thinkun@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-21 00:08:45 +0000

    Updated to 1.1.4
    git-svn-id: https://plugins.svn.wordpress.org/thinkun-remind/tags/1.1.4@627979 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ exportData.php
@@ -0,0 +1,91 @@
+<?php
+function fs_get_wp_config_path()
+{
+    $base = dirname(__FILE__);
+    $path = false;
+
+    if (@file_exists(dirname(dirname($base))."/wp-config.php"))
+    {
+        $path = dirname(dirname($base))."/wp-config.php";
+    }
+    else
+    if (@file_exists(dirname(dirname(dirname($base)))."/wp-config.php"))
+    {
+        $path = dirname(dirname(dirname($base)))."/wp-config.php";
+    }
+    else
+    $path = false;
+
+    if ($path != false)
+    {
+        $path = str_replace("\\", "/", $path);
+    }
+    return $path;
+}
+
+error_reporting(E_WARNING | E_PARSE);
+require_once(fs_get_wp_config_path()) ;
+global $wpdb;
+
+$file = "email";
+
+$select = "
+			SELECT 
+			e.create_date, 
+			e.email_id, 
+			e.template_id, 
+			t.template_name, 
+			e.to, 
+			e.name, 
+			e.subject, 
+			e.message, 
+			e.custom_fields
+			FROM 
+			".$wpdb->prefix."remind_email as e,
+			".$wpdb->prefix."remind_template as t 
+			WHERE 
+			e.template_id = t.template_id order by e.email_id ASC
+		";
+
+		
+$export = mysql_query ( $select ) or die ( "Sql error : " . mysql_error( ) );
+$fields = mysql_num_fields ( $export );
+
+for ( $i = 0; $i < $fields; $i++ )
+{	
+    $header .= mysql_field_name( $export , $i ) . ",";
+}
+
+$data="";
+while( $row = mysql_fetch_row( $export ) )
+{
+    $line = '';
+    foreach( $row as $value )
+    {                                            
+        if ( ( !isset( $value ) ) || ( $value == "" ) )
+        {
+            $value = ",";
+        }
+        else
+        {
+			$value = str_replace( '"' , '""' , $value );
+            $value = '"' . $value . '"' . ",";
+        }
+        $line .= $value;
+    }
+    $data .= trim( $line ) . "\n";
+}
+$data = str_replace( "\r" , "" , $data );
+if ( $data == "" )
+{
+    $data = "\n(0) Records Found!\n";                        
+}
+
+$filename = $file."_".date("Y-m-d_H-i",time());
+header("Content-type: application/vnd.ms-excel");
+header("Content-disposition: csv" . date("Y-m-d") . ".csv");
+header( "Content-disposition: filename=".$filename.".csv");
+print "$header\n$data";
+
+exit;
+?>
